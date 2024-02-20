e088a659445ffeff87faa0fba2493b5fa2d44615
missJin <kimhero91@naver.com>
2024-02-20 23:43:38 +0900

    포트폴리오 첫번째 커밋

---
+++ pool/js/main.js
@@ -0,0 +1,33 @@
+﻿$(function() {
+	
+	/* .mySwiper */
+    let swiper = new Swiper(".mySwiper", {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        pagination: {
+        el: ".swiper-pagination",
+		clickable: true,
+        }
+      });
+	  
+	/* 모달창 */  
+		const openModalButton = document.getElementById("openModal");
+        const modal = document.getElementById("myModal");
+        const closeModalButton = document.getElementById("closeModal");
+
+        openModalButton.addEventListener("click", function() {
+            modal.style.display = "block";
+        });
+
+        closeModalButton.addEventListener("click", function() {
+            modal.style.display = "none";
+        });
+
+        window.addEventListener("click", function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }); 
+});
---
+++ pool/js/special.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+
+	/* 이미지 슬라이드 */
+	let swiper = new Swiper(".mySwiper", {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        }
+      });
+	  
+});
